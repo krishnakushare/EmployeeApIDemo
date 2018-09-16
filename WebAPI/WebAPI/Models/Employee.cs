4206b6b8541c8ef3c22a31878200cc7f737da9eb
krishnakushare <krishkusharekk@gmail.com>
2018-09-16 12:01:39 +0530

    empapi

---
+++ WebAPI/WebAPI/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    [Table("Employee")]
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public int salary { get; set; }
+    }
+}
